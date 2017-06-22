2ac8538fb72833738a47598441e8b53fa5324594
lams3 <lams3@cin.ufpe.br>
2017-06-21 21:31:40 -0300

    Zoom

--- 3DSurface/3DSurface.js
+++ 3DSurface/3DSurface.js
@@ -12,10 +12,9 @@ function setupCanvas() {
     ctx.translate(canvas.width / 2, canvas.height / 2);
 }
 
-var rad = 0.0174533;
-var min = 1e9;
-var max = -1e9;
 function setup() {
+    var min = 1e9;
+    var max = -1e9;
     var params = prompt("Parameters separated by spaces:").split(" ");
     var func = Function(params, "return [" + prompt("X function") + ", " + prompt("Y function") + ", " + prompt("Z function") + "];");
     for (var i = 0; i < params.length; i++) {
@@ -25,7 +24,7 @@ function setup() {
       ranges[i][2] = Number(prompt("delta " + params[i]));
     }
 
-    (function calc(i, list) {
+    var calc = function(i, list) {
       if (i === ranges.length) {
         var s = "func(";
         for (var j = 0; j < list.length - 1; j++) {
@@ -42,7 +41,8 @@ function setup() {
         l.push(j);
         calc(i + 1, l);
       }
-    })(0, []);
+    };
+    calc(0, []);
 
     var delta = Math.abs(max - min) / colorMap.length;
 
@@ -50,43 +50,10 @@ function setup() {
         points[i].color = colorMap[Math.floor((points[i][2] - min) / delta)];
     }
 
-    frames = setInterval(() => {
+    setInterval(() => {
         draw();
     }, 1000 / 30);
 
-    /*var begin = -canvas.height / 3, end = canvas.height / 3;
-    var min = f(begin, begin);
-    var max = min;
-    for (var x = begin; x < end; x += 5) {
-        for (var y = begin; y < end; y += 5) {
-            points.push([x, y, f(x, y)]);
-            if (points[points.length - 1][2] < min) min = points[points.length - 1][2];
-            if (points[points.length - 1][2] > max) max = points[points.length - 1][2];
-        }
-    }
-
-    for (var u = 0; u < 360; u += 2) {
-        for (var v = -90; v < 90; v += 2) {
-            var x = r * Math.sin(u * rad) * Math.cos(v * rad);
-            var y = r * Math.cos(u * rad) * Math.cos(v * rad);
-            var z = r * Math.sin(v * rad);
-            points.push([x, y, z]);
-            if (points[points.length - 1][2] < min)
-                min = points[points.length - 1][2];
-            if (points[points.length - 1][2] > max)
-                max = points[points.length - 1][2];
-        }
-    }
-
-    var delta = Math.abs(max - min) / colorMap.length;
-
-    for (var i = 0; i < points.length; i++) {
-        points[i].color = colorMap[Math.floor((points[i][2] - min) / delta)];
-    }
-
-    frames = setInterval(() => {
-        draw();
-    }, 1000 / 30);*/
 }
 
 function matrixMult(op) {
@@ -112,7 +79,7 @@ function draw() {
 var colorMap = ["#060", "#090", "#0C0", "#0F0", "#9F0", "#9C0", "#990", "#960", "#930", "#900", "#C00"];
 var canvas;
 var ctx;
-var frames;
+var ranges = [];
 var points = [];
 var sin = Math.sin(0.0174533);
 var cos = Math.cos(0.0174533);
@@ -122,9 +89,10 @@ var rotation = {
     za: [[cos, sin, 0], [-sin, cos, 0], [0, 0, 1]],
     xh: [[1, 0, 0], [0, cos, -sin], [0, sin, cos]],
     yh: [[cos, 0, sin], [0, 1, 0], [-sin, 0, cos]],
-    zh: [[cos, -sin, 0], [sin, cos, 0], [0, 0, 1]]
+    zh: [[cos, -sin, 0], [sin, cos, 0], [0, 0, 1]],
+    zoomIn: [[1.01, 0, 0], [0, 1.01, 0], [0, 0, 1.01]],
+    zoomOut: [[0.99, 0, 0], [0, .99, 0], [0, 0, .99]]
 };
-var ranges = [];
 alert("Type the functions in JavaScript");
 alert("Keys Q, E, W, S, A and D rotate the surface");
 setupCanvas();
@@ -151,5 +119,13 @@ document.addEventListener("keydown", e => {
         case 83:
             matrixMult("xa");
             break;
+    }
+});
+
+document.addEventListener("wheel", e => {
+    if (e.deltaY > 0) {
+        matrixMult("zoomIn");
+    } else if (e.deltaY < 0){
+        matrixMult("zoomOut")
     }
 });
